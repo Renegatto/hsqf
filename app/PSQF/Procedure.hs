d19c51ae09808a6bf3ec25beb846477289165fb3
Alexey <edittorr@mail.ru>
2022-12-20 23:25:26 +0300

    Allow procedure fn to return Statement

--- app/PSQF/Procedure.hs
+++ app/PSQF/Procedure.hs
@@ -22,16 +22,16 @@ import Data.Kind (Constraint)
 import PSQF.HList (term, var)
 
 pprocedure ::
-  forall (ret :: PType) (args :: [PType]) (c :: Scope) (s :: S).
-  ( forall x. MatchArgs args ret x
-  , UnLamHasNext args
+  forall (ret :: PType) (c :: Scope) (args :: [PType]) (s :: S).
+  ( forall x. MatchArgs args ret c x
+  , UnLamHasNext args -- Needs to avoid GHC bug related to type inference (captured at f0507fcd60)
   ) =>
-  (forall x. Next args ret x) -> Term c s (args :==> ret)
+  (forall x. Next args ret c x) -> Term c s (args :==> ret)
 pprocedure f = MkTerm impl
   where
     impl :: forall (s' :: S). Int -> SQF
     impl lvl =
-      nextArg @args @ret @s' lvl [] f
+      nextArg @args @ret @c @s' lvl [] f
 
 type UnLambdaOf :: [PType] -> PType -> Scope -> PType
 type family UnLambdaOf args b c s = f | f -> args b c s where
@@ -40,12 +40,12 @@ type family UnLambdaOf args b c s = f | f -> args b c s where
 
 -- | Just a proof for GHC
 class UnLamHasNext xs where
-  unLamHasNext :: forall b s. Equal (UnLambdaOf xs b Expr s) (Next xs b s)
+  unLamHasNext :: forall b c s. Equal (UnLambdaOf xs b c s) (Next xs b c s)
 instance UnLamHasNext '[] where
   unLamHasNext = EqRefl
 instance UnLamHasNext as => UnLamHasNext (a:as) where
-  unLamHasNext :: forall b s. Equal (UnLambdaOf (a : as) b 'Expr s) (Next (a : as) b s)
-  unLamHasNext = liftEq $ unLamHasNext @as @b @s
+  unLamHasNext :: forall b c s. Equal (UnLambdaOf (a : as) b c s) (Next (a : as) b c s)
+  unLamHasNext = liftEq $ unLamHasNext @as @b @c @s
 
 data Equal a b where
   EqRefl :: forall a. Equal a a 
@@ -56,29 +56,29 @@ liftEq EqRefl = EqRefl
 eqCoerce :: Equal a b -> a -> b
 eqCoerce EqRefl x = x
 
-type MatchArgs :: [PType] -> PType -> S -> Constraint
-class MatchArgs xs b s where
-  type Next xs b s = c | c -> xs b s
-  nextArg :: Int -> [String] -> Next xs b s -> SQF
+type MatchArgs :: [PType] -> PType -> Scope -> S -> Constraint
+class MatchArgs xs b c s where
+  type Next xs b c s = r | r -> xs b c s
+  nextArg :: Int -> [String] -> Next xs b c s -> SQF
 
-instance MatchArgs xs b s => MatchArgs (x:xs) b s where
-  type Next (x:xs) b s = (Term Expr s x -> Next xs b s)
-  nextArg :: Int -> [String] -> (Term Expr s x -> Next xs b s) -> SQF
+instance MatchArgs xs b c s => MatchArgs (x:xs) b c s where
+  type Next (x:xs) b c s = (Term Expr s x -> Next xs b c s)
+  nextArg :: Int -> [String] -> (Term Expr s x -> Next xs b c s) -> SQF
   nextArg lvl vars f =
-    nextArg @xs @b @s (succ lvl) (mkVar lvl : vars) (f $ term $ var lvl)
+    nextArg @xs @b @c @s (succ lvl) (mkVar lvl : vars) (f $ term $ var lvl)
 
-instance MatchArgs '[] b s where
-  type Next '[] b s = Term Expr s b
-  nextArg :: Int -> [String] -> Term Expr s b -> SQF
+instance MatchArgs '[] b c s where
+  type Next '[] b c s = Term c s b
+  nextArg :: Int -> [String] -> Term c s b -> SQF
   nextArg lvl vars result =
     Procedure
       [ Call (GlobalVar "params") $ ListLit (LocalVar <$> vars)
       , runTerm result lvl
       ]
 
-inferenceExample = pprocedure @PInteger @'[PInteger,PString,PBool] $ \a b c ->
+inferenceExample = pprocedure @PInteger @Expr @'[PInteger,PString,PBool] $ \a b c ->
   a -- inferred
 
 reverseInferenceExample :: _
-reverseInferenceExample = pprocedure @PBool {- args type inferred -} $
+reverseInferenceExample = pprocedure @PBool @Expr {- args type inferred -} $
   \(a :: Term Expr x PInteger) (b :: Term Expr x PBool) (c :: Term Expr x PString) -> b
