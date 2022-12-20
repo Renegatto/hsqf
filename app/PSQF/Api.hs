1ed237ef36f5fd5f47349bce6120d43c9f15b30c
Alexey <edittorr@mail.ru>
2022-12-21 01:51:01 +0300

    Add more API things

--- app/PSQF/Api.hs
+++ app/PSQF/Api.hs
@@ -2,10 +2,11 @@
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE KindSignatures #-}
+{-# LANGUAGE StandaloneKindSignatures #-}
 module PSQF.Api where
 import PSQF.Definition 
 import PSQF.HList (pnil)
-import USQF (SQF(ListLit))
+import USQF (SQF(ListLit, StringLit))
 
 
 newtype PObject s = MkPObject { getObject :: Term Expr s PObject }
@@ -13,13 +14,26 @@ newtype PUnit s = MkPUnit { getUnit :: Term Expr s PUnit }
 newtype PPlayer s = MkPPlayer { getPlayer :: Term Expr s PPlayer }
 newtype PVehicle s = MkPVehicle { getVehicle :: Term Expr s PVehicle }
 
+type PEvent :: PType
+data PEvent s =
+  PFired | PHealed
+
+instance PCon PEvent where
+  pcon PFired = MkTerm $ \_ -> StringLit "fired"
+  pcon PHealed = MkTerm $ \_ -> StringLit "healed"
+
 -- | PSubtype a b means that a is subtype of b
 class PSubtype a b where
   pupcast :: Term Expr s a -> Term c s b
 instance PSubtype PUnit PObject where
   pupcast = punsafeCoerce
+instance PSubtype PVehicle PUnit where
+  pupcast = punsafeCoerce
 instance PSubtype PVehicle PObject where
   pupcast = punsafeCoerce
+
+punsafeDowncast :: PSubtype a b => Term Expr s b -> Term c s a
+punsafeDowncast = punsafeCoerce
 
 newtype PList (a :: PType) s = MkPList {getPList :: Term Expr s (PList a)}
 
