03a8dabaa1b56fa7e4df36b316a94c986188620b
Alexey <edittorr@mail.ru>
2023-01-02 15:45:23 +0300

    Make default generic impl for PCon'

--- app/HSQF/Language/Sum.hs
+++ app/HSQF/Language/Sum.hs
@@ -8,6 +8,8 @@
 {-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE NoMonomorphismRestriction #-}
 {-# LANGUAGE DeriveAnyClass #-}
+{-# LANGUAGE QuantifiedConstraints #-}
+{-# LANGUAGE DefaultSignatures #-}
 module HSQF.Language.Sum () where
 
 import Generics.SOP
@@ -19,41 +21,34 @@ import HSQF.Language.Definition (Term(runTerm, MkTerm))
 import SQF (SQF(ListLit, NumLit))
 import qualified GHC.Generics as GHC
 
-newtype DerivePSumViaData (pa :: PType) (s :: S) = MkDeriveViaData (pa s)
-
 undefined' :: forall a. a
 undefined' = undefined
 
 fromInt :: Int -> Float
 fromInt = fromIntegral -- need due GHC bug
 
-data ExampleSumType (s :: S)
-  = TheOnlyOne (Term 'Expr s PInteger)
-  | SecondOne (Term 'Expr s PString)
-  deriving stock GHC.Generic
-  deriving anyclass Generic
+class PCon' (a :: PType) where
+  pcon' :: a s -> Term c s a
+  default pcon' :: 
+    forall pass s c.
+    ( Generic (a s)
+    , AllZip (IsSingletonProduct s) (Code (a s)) pass
+    ) => a s -> Term c s a
+  pcon' = gpcon @a @pass @s @c 
 
-case1 :: ExampleSumType s
-case1 = TheOnlyOne (pconstant @Integer 200)
-
-case2 :: ExampleSumType s
-case2 = SecondOne (pconstant "Some string")
-
-compiledCase1 = gpcon (MkDeriveViaData case1)
-compiledCase2 = gpcon (MkDeriveViaData case2)
--- >>> (compile compiledCase1, compile compiledCase2)
--- ("[0.0,200.0];","[1.0,\"Some string\"];")
-
-gpcon :: forall (pa :: PType) (pass :: [PType]) (ass :: [[Type]]) s c.
+gpcon ::
+  forall (pa :: PType) (pas :: [PType]) s c.
   ( Generic (pa s)
-  , ass ~ Code (pa s)
-  , AllZip (IsSingletonProduct s) ass pass
-  ) => DerivePSumViaData pa s -> Term c s (DerivePSumViaData pa)
-gpcon (MkDeriveViaData x) = unwrap @ass @pass (from x) 0
+  , AllZip (IsSingletonProduct s) (Code (pa s)) pas
+  ) =>
+  pa s ->
+  Term c s pa
+gpcon x =
+    unwrap @(Code (pa s)) @pas (from x) 0
   where
     unwrap :: forall (xss :: [[Type]]) (pxs :: [PType]).
       AllZip (IsSingletonProduct s) xss pxs =>
-      SOP I xss -> Int -> Term c s (DerivePSumViaData pa)
+      SOP I xss -> Int -> Term c s pa
     unwrap (SOP y) (conId :: Int) = case y of
       (Z (I caseTerm :* Nil)) -> MkTerm $ \lvl -> 
         let compiled = runTerm caseTerm lvl
@@ -65,6 +60,7 @@ gpcon (MkDeriveViaData x) = unwrap @ass @pass (from x) 0
 class (a ~ Term 'Expr s pa) => IsTerm s a pa
 class (xs ~ '[Term 'Expr s pa]) => IsSingletonProduct s xs pa
 instance (xs ~ '[Term 'Expr s pa]) => IsSingletonProduct s xs pa
+
 transTerms ::
   forall s (as :: [Type]) (pas :: [PType]).
   AllZip (IsTerm s) as pas =>
@@ -74,3 +70,23 @@ transTerms =
   trans_NP
     (Proxy :: Proxy (IsTerm s))
     (\(I x) -> x)
+
+data ExampleSumType (s :: S)
+  = TheOnlyOne (Term 'Expr s PInteger)
+  | SecondOne (Term 'Expr s PString)
+  deriving stock GHC.Generic
+  deriving anyclass Generic
+
+deriving anyclass instance (PCon' ExampleSumType)
+
+case1 :: ExampleSumType s
+case1 = TheOnlyOne (pconstant @Integer 200)
+
+case2 :: ExampleSumType s
+case2 = SecondOne (pconstant "Some string")
+
+compiledCase1 = pcon' case1
+compiledCase2 = pcon' case2
+-- >>> (compile compiledCase1, compile compiledCase2)
+-- ("[0.0,200.0];","[1.0,\"Some string\"];")
+
